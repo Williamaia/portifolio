0500c623e73cace50dd5cf8ca0b678243fdb13e6
William Maia <williamaiadias@hotmail.com>
2019-06-07 22:05:19 -0300

    Updates

---
+++ precache-manifest.88100836d714312453678fb3ec31bbec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acf395113ca827bea52bb9f3beb2454",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "74cc66a4429d915ca058",
+    "url": "/portifolio/static/css/main.84f00e22.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portifolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "74cc66a4429d915ca058",
+    "url": "/portifolio/static/js/main.e4465802.chunk.js"
+  },
+  {
+    "revision": "91a74ff0904045e04265",
+    "url": "/portifolio/static/js/runtime~main.e9cc7322.js"
+  }
+]);
